d6734de63c60ec76526b7ea9989a42a26fc64613
lionDev <109150445+lionDev28@users.noreply.github.com>
2022-09-16 15:00:53 +0500

    add files

---
+++ main.js
@@ -0,0 +1,29 @@
+var input = document.querySelector('.input');
+var showResult = document.querySelector('.show-result');
+var result = document.querySelector('.result');
+
+showResult.addEventListener('click', frg)
+function frg() {
+
+	let frginput = input.value;
+
+	let frgResult = (5 / 9) * (frginput - 32);
+
+	result.innerHTML = "C is: " + frgResult;
+};
+
+
+var inputC = document.querySelector('.input-c');
+var showResultC = document.querySelector('.show-result-c');
+var resultC = document.querySelector('.result-c');
+
+showResultC.addEventListener('click', celcius);
+
+function celcius() {
+
+	let cinput = inputC.value;
+
+	let cresult = (cinput * 1.8) + 32;
+
+	resultC.innerHTML = "F is:" + cresult;
+};
